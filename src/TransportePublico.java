70f46ae24fe2873b8c73ba1bd1c765ad4df02eb8
ceciliaromero-creator <cecilia.romero@fiuni.edu.py>
2026-04-17 14:23:29 -0300

    Update TransportePublico.java
    Agregado de documentacion

--- src/TransportePublico.java
+++ src/TransportePublico.java
@@ -1,9 +1,19 @@
+/**
+ * La clase TransportePublico representa un tipo de vehículo destinado
+ * al traslado de pasajeros, heredando las características generales de Vehiculo.
+ */
 public class TransportePublico extends Vehiculo{
-    
+    /**
+     * Constructor que inicializa el transporte público con un identificador único.
+     * @param id Identificador del vehículo.
+     */
     public TransportePublico(int id){
         super(id);
     }
-    
+     /**
+     * Define el patrón de movimiento específico del transporte público.
+     * Este comportamiento sobrescribe el método de la clase base Vehiculo.
+     */
     @Override
     public void patronMovimiento(){
         System.out.println("El transporte avanza de forma segura con los pasajeros a bordo.");
