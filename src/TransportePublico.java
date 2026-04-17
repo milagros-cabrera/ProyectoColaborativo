3c956de1afdd46b8e2063dc17d071ee1146b3c72
ceciliaromero-creator <cecilia.romero@fiuni.edu.py>
2026-04-17 11:49:11 -0300

    Update TransportePublico.java

--- src/TransportePublico.java
+++ src/TransportePublico.java
@@ -1,4 +1,9 @@
 public class TransportePublico extends Vehiculo{
+    
+    public TransportePublico(int id){
+        super(id);
+    }
+    
     @Override
     public void patronMovimiento(){
         System.out.println("El transporte avanza de forma segura con los pasajeros a bordo.");
